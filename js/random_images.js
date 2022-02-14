23812c399f03e30e5a0d6f93f3cc564b40f78c43
molodchy.ilya <molodchy.ilya@yandex.ru>
2022-02-14 15:43:38 +0300

    Task4
    Изменены имена, вставка картинок, структура scss

--- js/random_images.js
+++ js/random_images.js
@@ -1,4 +1,4 @@
-const images = [...document.querySelectorAll('.item__img')];
+const images = [...document.querySelectorAll('.card__img-container')];
 const colors = ['#081580', '#d3d8e4', '#ff779a', '#070707'];
 
 images.map((image) => {
