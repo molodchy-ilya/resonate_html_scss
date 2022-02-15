f19f3a24af0b550551c40e207fd663f727c853a7
molodchy.ilya <molodchy.ilya@yandex.ru>
2022-02-15 10:50:55 +0300

    Footer fixes

--- js/random_images.js
+++ js/random_images.js
@@ -1,5 +1,5 @@
 const images = [...document.querySelectorAll('.card__img-container')];
-const colors = ['#081580', '#d3d8e4', '#ff779a', '#070707'];
+const colors = ['#083090', '#d3d8e4', '#ff779a', '#070707'];
 
 images.map((image) => {
   image.style.backgroundColor =
