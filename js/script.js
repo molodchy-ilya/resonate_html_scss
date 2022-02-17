ef344c4fbde5aed6d06d13aee1c752cda6ac2c32
molodchy.ilya <molodchy.ilya@yandex.ru>
2022-02-17 16:49:02 +0300

    header/nav fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// show/hide menu
 const burger = document.querySelector('.header__burger');
 if (burger) {
   burger.addEventListener('click', () => {
@@ -5,5 +6,18 @@ if (burger) {
     if (menu) {
       menu.classList.toggle('menu_show');
     }
+  });
+}
+
+// change active link
+const links = document.querySelectorAll('.menu__link');
+if (links) {
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      const currentActive = document.querySelector('.menu__link_active');
+      currentActive.classList.remove('menu__link_active');
+
+      link.classList.add('menu__link_active');
+    });
   });
 }
